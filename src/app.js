d5c1d0458ffde4f9c6ceb7059e2203068c69243b
Victoria Kovalevska <vvjj02.95@gmail.com>
2023-06-20 12:46:11 +0200

    improved styling

--- src/app.js
+++ src/app.js
@@ -70,11 +70,9 @@ function displayForecast(response) {
   forecastElement.innerHTML = forecastHTML;
 }
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "bd5b4461863eddaa6ced0a0a67989e0a";
 
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
